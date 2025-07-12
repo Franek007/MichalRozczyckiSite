3e306e63671d7f5f6f39429d3a00d0cb4f8c546e
Franek007 <158212895+Franek007@users.noreply.github.com>
2025-07-12 10:22:43 +0200

    added carousel

--- src/js/landingPage.js
+++ src/js/landingPage.js
@@ -3,6 +3,7 @@ let opinionsProgressDots
 let opinionsBtnRight
 let opinionsBtnLeft
 let opinionsBtns
+let opinionsSlider
 
 const main2 = () => {
 	prepareDOMElements()
@@ -15,6 +16,7 @@ const prepareDOMElements = () => {
 	opinionsBtnLeft = document.querySelector('.opinions__btn--left')
 	opinionsBtnRight = document.querySelector('.opinions__btn--right')
 	opinionsBtns = document.querySelectorAll('.opinions__btn')
+	opinionsSlider = document.querySelector('.opinions__slider')
 }
 
 const prepareDOMEvents = () => {
@@ -25,22 +27,62 @@ const prepareDOMEvents = () => {
 			btn.addEventListener('click', handleOpinionRightSlider)
 		}
 	})
+	handleOpinionsDot()
 }
 
-const handleOpinionLeftSlider = params => {}
+const handleOpinionLeftSlider = () => {
+	handleActiveCard('left', -1)
+}
 
-const handleOpinionRightSlider = params => {}
+const handleOpinionRightSlider = () => {
+	handleActiveCard('right', 1)
+}
 
-const handleOpinionSlider = () => {
-	opinionsCards.forEach(card => {
-		if (card.classList.contains('opinions__card--active')) {
-			console.log('jej')
-			card.style.transfrom = 'translateX(-300px)'
-			const activeCard = card.dataset.cardNumber
-			let activeDot = Array.from(opinionsProgressDots).find(dot => dot.dataset.cardNumber == activeCard)
-			activeDot.classList.add('.opinions__progress-dot--active')
-		}
+const handleOpinionsDot = () => {
+	opinionsProgressDots.forEach(dot => {
+		dot.addEventListener('click', () => {
+			const targetNumber = parseInt(dot.dataset.cardNumber)
+			const activeCard = document.querySelector('.opinions__card--active')
+			const currentNumber = parseInt(activeCard.dataset.cardNumber)
+			const diff = targetNumber - currentNumber
+
+			if (diff !== 0) {
+				handleActiveCard(diff > 0 ? 'right' : 'left', Math.abs(diff))
+			}
+		})
 	})
+}
+
+const handleActiveCard = (direction, value) => {
+	let activeCard = Array.from(opinionsCards).find(card => card.classList.contains('opinions__card--active'))
+
+	const activeCardDataset = activeCard.dataset.cardNumber
+	activeCard.classList.remove('opinions__card--active')
+
+	let newCardNumber = parseInt(activeCardDataset) + value
+
+	if (newCardNumber > 3) newCardNumber = 1
+	if (newCardNumber < 1) newCardNumber = 3
+
+	// if (direction == 'right') {
+	// 	activeCard.style.transform = 'translateX(-300px)'
+	// } else {
+	// 	activeCard.style.transform = 'translateX(300px)'
+	// }
+
+	const offset = (newCardNumber - 1) * 320 // lub dynamicznie: opinionsCards[0].offsetWidth
+	opinionsSlider.style.transform = `translateX(-${offset}px)`
+
+	const newActiveCard = Array.from(opinionsCards).find(card => parseInt(card.dataset.cardNumber) === newCardNumber)
+	newActiveCard.classList.add('opinions__card--active')
+
+	const activeDot = Array.from(opinionsProgressDots).find(
+		dot => parseInt(dot.dataset.cardNumber) === parseInt(activeCardDataset)
+	)
+	activeDot.classList.remove('opinions__progress-dot--active')
+
+	const newActiveDot = Array.from(opinionsProgressDots).find(dot => parseInt(dot.dataset.cardNumber) === newCardNumber)
+	newActiveDot.classList.add('opinions__progress-dot--active')
 }
 
 document.addEventListener('DOMContentLoaded', main2)
